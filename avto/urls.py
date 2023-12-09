fa7f70512b0ff264b740c169bcb1835ae784c675
sarvinozsaidova <134921413+sarvinozsaidova@users.noreply.github.com>
2023-12-09 20:43:11 +0300

    commit

--- avto/urls.py
+++ avto/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import AvtoListView, AvtoDetailView, AvtoDeleteView, AvtoCreateView, AvtoUpdateView
 
 urlpatterns = [
-    path('avtos/', views.avtos, name='avtos'),
-    path('avto/<str:pk>', views.avto, name='avto'),
-    path('avtodelete/<str:pk>', views.avtodelete, name='avtodelete'),
-    path('avtoupdate/<str:pk>', views.avtoupdate, name='avtoupdate'),
+    path('avtos/', AvtoListView.as_view(), name='avtos'),
+    path('avto/<str:pk>', AvtoDetailView.as_view(), name='avto'),
+    path('avto/delete/<str:pk>', AvtoDeleteView.as_view(), name='avto'),
+    path('avto/create/', AvtoCreateView.as_view(), name='avtocreate'),
+    path('avto/update/<str:pk>', AvtoUpdateView.as_view(), name='avto'),
+    # path('avto/<str:pk>', views.avto, name='avto'),
+    # path('avtodelete/<str:pk>', views.avtodelete, name='avtodelete'),
+    # path('avtoupdate/<str:pk>', views.avtoupdate, name='avtoupdate'),
 ]
--- avto/views.py
+++ avto/views.py
@@ -1,27 +1,83 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
 from .models import Avtomobile
 from .forms import AvtoForm
+from django.views import View
+from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
 
-def avtos(request):
-    avtos = Avtomobile.objects.all()
-    return render(request=request, template_name='avtos.html', context={'avtos':avtos})
 
-def avto(request, pk):
-    avto = get_object_or_404(Avtomobile, id=pk)
-    return render(request=request, template_name='avto.html', context={'avto':avto})
+class AvtoListView(ListView):
+    model = Avtomobile
+    template_name = 'avtos.html'
+    context_object_name = 'avtos'
 
-def avtodelete(request, pk):
-    avto = get_object_or_404(Avtomobile, id=pk)
-    avto.delete()
-    return redirect('/avtos')
+    def get_context_date(self, **kwargs):
+        context = super(AvtoListView, self).get_context_data(**kwargs)
+        avtos = Avtomobile.objects.all()
+        context['avtos'] = avtos
+        return context
+    
+class AvtoDetailView(DetailView):
+    model = Avtomobile
+    template_name = 'avto.html'
+    context_object_name = 'avto'
 
-def avtoupdate(request, pk):
-    avto = get_object_or_404(Avtomobile, id=pk)
-    form = AvtoForm(initial={"model":avto.model, "year":avto.year, "price":avto.price, "color":avto.color})
 
-    if request.method == 'AVTOMOBILE':
-        form = AvtoForm(request.AVTOMOBILE, instance=avto)
-        if form.is_valid():
-            form.save()
-            return redirect('/posts')
-    return render(request, 'update.html', {'form':form})
+class AvtoDeleteView(DeleteView):
+    model = Avtomobile
+    template_name = 'delete.html'
+    success_url = reverse_lazy('avtos')
+
+class AvtoCreateView(CreateView):
+    model = Avtomobile
+    template_name = 'create.html'
+    fields = ('model', 'year', 'price', 'color', 'image')
+    success_url = reverse_lazy('avtos')
+
+class AvtoUpdateView(UpdateView):
+
+    model = Avtomobile
+    template_name = 'update.html'
+    context_object_name = 'avto'
+    fields = ('model', 'year', 'price', 'color', 'image')
+
+    def get_success_url(self):
+        return reverse_lazy('avto', kwargs={'pk': self.object.id})
+
+
+    # def get_context_date(self, pk, **kwargs):
+    #     context = super(Avtomobile, self).get_context_data(**kwargs)
+    #     avto = get_object_or_404(Avtomobile, id=pk)
+    #     context['avto'] = avto
+    #     return context
+
+
+# class AvtoDetailView(View):
+#     def get(self, request, pk):
+#         avto = get_object_or_404(Avtomobile, id=pk)
+#         return render(request, 'avto.html', {'avto':avto})
+
+
+# def avtos(request):
+#     avtos = Avtomobile.objects.all()
+#     return render(request=request, template_name='avtos.html', context={'avtos':avtos})
+
+# def avto(request, pk):
+#     avto = get_object_or_404(Avtomobile, id=pk)
+#     return render(request=request, template_name='avto.html', context={'avto':avto})
+
+# def avtodelete(request, pk):
+#     avto = get_object_or_404(Avtomobile, id=pk)
+#     avto.delete()
+#     return redirect('/avtos')
+
+# def avtoupdate(request, pk):
+#     avto = get_object_or_404(Avtomobile, id=pk)
+#     form = AvtoForm(initial={"model":avto.model, "year":avto.year, "price":avto.price, "color":avto.color})
+
+#     if request.method == 'AVTOMOBILE':
+#         form = AvtoForm(request.AVTOMOBILE, instance=avto)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('/posts')
+#     return render(request, 'update.html', {'form':form})
